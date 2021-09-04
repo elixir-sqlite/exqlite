3c7b69871baab8be66dda6e0cda62102a3354969
Roman Heinrich <roman.heinrich@gmail.com>
2021-09-04 20:07:47 +0200

    Update to 3.36.0 + update script (closes #152) (#159)
    * Little script to update SQLite
    
    * Update Sqlite to recent stable version (3.36.0)
    
    * Remove `-DSQLITE_ENABLE_DESERIALIZE=1` as this is enabled by default now
    
    * Also remove `DSQLITE_ENABLE_DESERIALIZE=1` in Makefile.win

---
+++ bin/download_sqlite.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash 
+
+# https://sqlite.org/chronology.html
+# https://sqlite.org/download.html
+# https://www.sqlite.org/src/timeline?r=version-3.36.0
+# VERSION consists of 7 digits, like:3350000 for "3.35.5" or 3360000 for "3.36.0"
+
+VERSION=3360000 # 3.36.0
+
+mkdir -p tmp
+pushd tmp
+
+wget https://sqlite.org/2021/sqlite-autoconf-$VERSION.tar.gz
+
+tar xvfz sqlite-autoconf-$VERSION.tar.gz
+cd sqlite-autoconf-$VERSION
+
+cp sqlite3.c ../../sqlite3/
+cp sqlite3.h ../../sqlite3/
+cp sqlite3ext.h ../../sqlite3/
+
+rm -rf tmp/*
+
+echo "UPDATED SQLITE C CODE TO $VERSION!"
