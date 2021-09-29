a42d3a50c6544781aa650fdb0e5844159df6295e
Dominic Letz <dominic@diode.io>
2021-09-29 13:24:39 +0200

    Allow building static nifs

--- c_src/sqlite3_nif.c
+++ c_src/sqlite3_nif.c
@@ -857,4 +857,10 @@ static ErlNifFunc nif_funcs[] = {
   {"enable_load_extension", 2, exqlite_enable_load_extension, ERL_NIF_DIRTY_JOB_IO_BOUND},
 };
 
+// Elixir workaround for . in module names
+#ifdef STATIC_ERLANG_NIF
+#undef ERL_NIF_INIT_DECL
+#define ERL_NIF_INIT_DECL(MODNAME) ErlNifEntry* sqlite3_nif_nif_init(ERL_NIF_INIT_ARGS)
+#endif
+
 ERL_NIF_INIT(Elixir.Exqlite.Sqlite3NIF, nif_funcs, on_load, NULL, NULL, NULL)
