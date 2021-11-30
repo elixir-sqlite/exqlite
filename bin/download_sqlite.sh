65a6546083c0fa4879f43646ce65fd0ada5f8feb
Matthew Johnston <warmwaffles@gmail.com>
2021-11-30 07:35:41 -0600

    Bump sqlite to version 3.37.0

--- bin/download_sqlite.sh
+++ bin/download_sqlite.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash 
 
+set -e
+
 # https://sqlite.org/chronology.html
 # https://sqlite.org/download.html
 # https://www.sqlite.org/src/timeline?r=version-3.36.0
 # VERSION consists of 7 digits, like:3350000 for "3.35.5" or 3360000 for "3.36.0"
 
-VERSION=3360000 # 3.36.0
+VERSION=3370000 # 3.37.0
 
 mkdir -p tmp
 pushd tmp
@@ -13,12 +15,13 @@ pushd tmp
 wget https://sqlite.org/2021/sqlite-autoconf-$VERSION.tar.gz
 
 tar xvfz sqlite-autoconf-$VERSION.tar.gz
-cd sqlite-autoconf-$VERSION
 
-cp sqlite3.c ../../c_src/
-cp sqlite3.h ../../c_src/
-cp sqlite3ext.h ../../c_src/
+cp sqlite-autoconf-$VERSION/sqlite3.c ../c_src/
+cp sqlite-autoconf-$VERSION/sqlite3.h ../c_src/
+cp sqlite-autoconf-$VERSION/sqlite3ext.h ../c_src/
 
-rm -rf tmp/*
+rm -rf sqlite-autoconf-*
+
+popd
 
 echo "UPDATED SQLITE C CODE TO $VERSION!"
