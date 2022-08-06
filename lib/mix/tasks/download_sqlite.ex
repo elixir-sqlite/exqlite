493e5650cd8f0a5090e330d3bbca1e68f9ca4447
Greg Rychlewski <greg.rychlewski@gmail.com>
2022-08-06 14:41:06 -0400

    better variable naming

--- lib/mix/tasks/download_sqlite.ex
+++ lib/mix/tasks/download_sqlite.ex
@@ -9,8 +9,8 @@ defmodule Mix.Tasks.DownloadSqlite do
     System.cmd("sh", ["-c", "VERSION=#{version} bin/download_sqlite.sh"])
   end
 
-  defp tarball_version(version) do
-    [major, minor, patch] = String.split(version, ".")
+  defp tarball_version(semantic_version) do
+    [major, minor, patch] = String.split(semantic_version, ".")
     minor = String.pad_leading(minor, 2, "0")
     patch = String.pad_leading(patch, 2, "0")
 
