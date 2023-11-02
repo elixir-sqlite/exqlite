40b3658e2533ac69d6e8413aa2829f937b8121c0
Jason <jason@fly.io>
2023-11-02 14:02:46 -0500

    Update lib/exqlite/connection.ex
    Co-authored-by: Matthew Johnston <warmwaffles@gmail.com>

--- lib/exqlite/connection.ex
+++ lib/exqlite/connection.ex
@@ -159,7 +159,9 @@ defmodule Exqlite.Connection do
             "./priv/sqlite/\#{arch_dir}/vss0"
           ]
       ```
-    * `:before_disconnect` - A function to run before disconnect, either a 2-arity fun or `{module, function, args}` with the close reason and `t:Exqlite.Connection.t/0` prepended to `args` or `nil` (default: `nil`)
+    * `:before_disconnect` - A function to run before disconnect, either a 
+      2-arity fun or `{module, function, args}` with the close reason and 
+      `t:Exqlite.Connection.t/0` prepended to `args` or `nil` (default: `nil`)
 
   For more information about the options above, see [sqlite documentation][1]
 
