e55cdf9e420c0b2c6a64eebc04700ebdd703e6cf
Matthew Johnston <warmwaffles@gmail.com>
2024-09-12 08:02:33 -0500

    Update download sqlite shell script to accept arg

--- bin/download_sqlite.sh
+++ bin/download_sqlite.sh
@@ -6,20 +6,25 @@ set -e
 # https://sqlite.org/download.html
 # https://www.sqlite.org/src/timeline?r=version-3.36.0
 # VERSION consists of 7 digits, like:3350000 for "3.35.5" or 3360000 for "3.36.0"
+#
+# Execute with:
+#
+#   bin/download_sqlite.sh 3460100
+#
 
 mkdir -p tmp
 pushd tmp
 
-wget https://sqlite.org/2024/sqlite-autoconf-$VERSION.tar.gz
+wget https://sqlite.org/2024/sqlite-autoconf-$1.tar.gz
 
-tar xvfz sqlite-autoconf-$VERSION.tar.gz
+tar xvfz sqlite-autoconf-$1.tar.gz
 
-cp sqlite-autoconf-$VERSION/sqlite3.c ../c_src/
-cp sqlite-autoconf-$VERSION/sqlite3.h ../c_src/
-cp sqlite-autoconf-$VERSION/sqlite3ext.h ../c_src/
+cp sqlite-autoconf-$1/sqlite3.c ../c_src/
+cp sqlite-autoconf-$1/sqlite3.h ../c_src/
+cp sqlite-autoconf-$1/sqlite3ext.h ../c_src/
 
 rm -rf sqlite-autoconf-*
 
 popd
 
-echo "UPDATED SQLITE C CODE TO $VERSION!"
+echo "UPDATED SQLITE C CODE TO $1!"
